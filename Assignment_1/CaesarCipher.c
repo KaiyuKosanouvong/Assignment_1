1d674d32f06d4a06bd49eb8b4c7d5f71aec1e7cf
Kaiyu Kosanouvong <kak8903@rit.edu>
2024-09-11 20:32:03 -0400

    revised, still broken but will keep working

--- Assignment_1/CaesarCipher.c
+++ Assignment_1/CaesarCipher.c
@@ -3,15 +3,15 @@
 
 // encrypts the file contents 5 characters back
 static void cEncrypt(FILE* file, char cipherFilename []) {
-	char * code[256];
-	char * buff[256];
+	char code[256];
+	char buff[256];
 	// index counter
 	int i = 0;
 
+	// read one character
+	fread(buff, sizeof(char), 1, file);
 	// loop until end of file
 	while (!feof(file)) {
-		// read one character
-		fread(buff, sizeof(buff), 1, file);
 		// store character
 		char ch = buff[i];
 
@@ -20,41 +20,47 @@ static void cEncrypt(FILE* file, char cipherFilename []) {
 
 		// increment counter
 		i++;
+		// read next character
+		fread(buff, sizeof(char), 1, file);
 	}
 
 	// copy code to cipher file
-	FILE * ciphFile = fopen(cipherFilename, 'w');
+	FILE * ciphFile = fopen(cipherFilename, "w");
 	int eSize = sizeof(code[0]);
-	fwrite(code, sizeof(code), eSize, ciphFile);
+	fwrite(code, i, eSize, ciphFile);
 	fclose(ciphFile);
 }
 
 // decrypts the file contents to its original message
 // same algorithm as encrypt just reverse to go up 5 characters
 static void cDecrypt(FILE * code, char plainFilename []) {
-	char * og[256];
-	char * buff[256];
+	char og[256];
+	char buff[256]; 
 	// index counter
 	int i = 0;
 
+	// read one character
+	fread(buff, sizeof(char), 1, code);
 	// loop until end of file
 	while (!feof(code)) {
 		// read one character
-		fread(buff, sizeof(buff), 1, code); 
+		fread(buff, sizeof(char), 1, code); 
 		// store character
 		char ch = buff[i];
 
 		// decrypt character
-		og[i] = ((ch - 'A' + 5 + 26) % 26) + 'A'; 
+		og[i] = ch + 5; 
 		
 		// increment counter
 		i++; 
+		// read next character
+		fread(buff, sizeof(char), 1, code);
 	}
 
 	// copy code to 'original' file
-	FILE* plainFile = fopen(plainFilename, 'w');
+	FILE* plainFile = fopen(plainFilename, "w");
 	int eSize = sizeof(og[0]);
-	fwrite(og, sizeof(og), eSize, plainFile);
+	fwrite(og, i, eSize, plainFile);
 	fclose(plainFile);
 }
 
@@ -76,7 +82,7 @@ void main(void) {
 	scanf("%s", cipherFilename);
 	
 	// open file for reading access
-	file = fopen(filename, 'r');
+	file = fopen(filename, "r");
 
 	// start encryption
 	printf("encrypting...");
@@ -84,7 +90,7 @@ void main(void) {
 	
 	// start decryption
 	printf("decrypting...");
-	FILE * cFile = fopen(cipherFilename, 'r');
+	FILE * cFile = fopen(cipherFilename, "r");
 	cDecrypt(cFile, plainFilename);
 
 	fclose(file);
